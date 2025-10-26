d826fd502bcef126b96c758d1085d2789239b598
Aditya-SE <adityapatil849703801313@gmail.com>
2025-10-26 19:12:18 +0530

    update js file

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 const question = document.getElementById("question");
 const num1 = Math.floor(Math.random() * 10) + 1;
 const num2 = Math.floor(Math.random() * 10) + 1;
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input");
 
-question.innerText = `what is ${num1} multiply by ${num2}?`
+question.innerText = `what is ${num1} multiply by ${num2}?`;
+
+let score = 0;
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit", ()=>{
+    const userAns = +inputEl.value;
+    if(userAns == correctAns){
+        score++;
+        
+    }else{
+        score--;
+        
+    }
+});
+
+
